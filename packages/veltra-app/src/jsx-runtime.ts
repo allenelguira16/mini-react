72f30b31d751f30a684c0089beb5e85f119ac55a
Michael Allen Elguira <allenelguira16@gmail.com>
2025-06-22 19:58:43 +0800

    feat: v0.0.0

--- packages/veltra-app/src/jsx-runtime.ts
+++ packages/veltra-app/src/jsx-runtime.ts
@@ -4,7 +4,14 @@ import { toArray } from "./util";
 
 import "./jsx.d";
 
-// Transform jsx to use h
+/**
+ * jsx runtime
+ *
+ * @param type - The type of the element.
+ * @param props - The properties of the element.
+ * @param children - The children of the element.
+ * @returns The JSX element.
+ */
 const jsx = (type: any, { children = [], ...props }: any) => {
   if (IS_SSR) {
     return hSSR(type, props, toArray(children));
